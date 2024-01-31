bb475428655150313445b8192094426ff1c91650
“c.b.olha” <c.b.olha@viseven.com>
2024-01-31 15:24:23 -0300

    added hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import "../styles/Hero.css";
+import photoGrid from "../assets/photo-grid.png";
+
+const Hero = () => {
+  return (
+    <section className="hero-section">
+      <img src={photoGrid} alt="" className="hero-photo-grid" />
+      <h1 className="hero-title">Online experiences</h1>
+      <p className="hero-description">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
