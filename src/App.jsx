f7362875b57c84a8900abff5d6816c6657d513e7
“c.b.olha” <c.b.olha@viseven.com>
2024-02-01 17:23:55 -0300

    added cards container section

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
+import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
 import data from "./data.js";
-
-console.log(data);
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
-      {data.map((item) => (
-        <Card
-          key={item.id}
-          img={item.coverImg}
-          rating={item.stats.rating}
-          reviewCount={item.stats.reviewCount}
-          location={item.location}
-          title={item.title}
-          price={item.price}
-        />
-      ))}
+      <section className="cards-container">
+        {data.map((item) => (
+          <Card
+            key={item.id}
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+          />
+        ))}
+      </section>
     </>
   );
 };
